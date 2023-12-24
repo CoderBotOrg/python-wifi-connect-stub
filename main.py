d244678d3970ae0e5b479176d7560c467b10ef0b
previ <roberto.previtera@gmail.com>
2023-12-24 10:13:59 +0000

    connexion 3

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 import logging
 import logging.handlers
 import connexion
-
-from flask_cors import CORS
+from connexion.middleware import MiddlewarePosition
+from starlette.middleware.cors import CORSMiddleware
 
 # Logging configuration
 logger = logging.getLogger()
@@ -15,19 +15,21 @@ logger.setLevel(os.environ.get("LOGLEVEL", "INFO"))
 
 # Serve a custom version of the swagger ui (Jinja2 templates) based on the default one
 #  from the folder 'swagger-ui'. Clone the 'swagger-ui' repository inside the backend folder
-options = {"swagger_ui": False}
-connexionApp = connexion.App(__name__, options=options)
 
-# Connexion wraps FlaskApp, so app becomes connexionApp.app
-app = connexionApp.app
-# Access-Control-Allow-Origin
-CORS(app)
-app.debug = False
+app = connexion.App(__name__)
+app.add_middleware(
+    CORSMiddleware,
+    position=MiddlewarePosition.BEFORE_EXCEPTION,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 ## New API and web application
 
 # API v1 is defined in v1.yml and its methods are in api.py
-connexionApp.add_api('v1.yml')
+app.add_api('v1.yml')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9090, debug=True, use_reloader=False, threaded=True)
+    app.run(host="0.0.0.0", port=9090)
